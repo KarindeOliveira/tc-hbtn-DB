7093c2d4ac60a881dcca14c96eb2e1785363839a
Karin Pinheiro <karindanoli@gmail.com>
2022-07-19 16:38:52 -0300

    first commit

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE CLIENTE (id int NOT NULL PRIMARY KEY, nome TEXT NOT NULL, email TEXT NOT NULL, telefone int);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
