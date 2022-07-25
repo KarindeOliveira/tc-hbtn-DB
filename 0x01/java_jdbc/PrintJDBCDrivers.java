b3323139d90efc17b19bbd9f68dc85a9a594798b
Karin Pinheiro <karindanoli@gmail.com>
2022-07-25 15:12:50 -0300

    Teste

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -3,19 +3,20 @@ import java.sql.DriverManager;
 import java.util.Enumeration;
 
 public class PrintJDBCDrivers {
+
     public static void main(String[] args) {
 
         System.out.println("Lista de todos os drivers dos diferentes fabricantes: ");
         for (Enumeration<Driver> driverEnumeration = DriverManager.getDrivers();
              driverEnumeration.hasMoreElements();){
             Driver driver = driverEnumeration.nextElement();
-            dadosDriver(driver);
+            print(driver);
         }
 
-}
+    }
 
-    private static void dadosDriver(Driver driver) {
-        String nome = Driver.class.getName();
+    private static void print(Driver driver) {
+        String nome = driver.getClass().getName();
         int versaoMajor = driver.getMajorVersion();
         int versaoMinor = driver.getMinorVersion();
         System.out.println("Nome: " + nome);
